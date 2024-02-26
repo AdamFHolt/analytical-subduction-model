6978f65472fcad80440c394409b24e8026cb5fa6
aholt@rsmas.miami.edu <aholt@rsmas.miami.edu>
2024-02-26 11:20:03 -0500

    Improved function structure

--- function.py
+++ function.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
 
-def some_function():
-	...
-	return(???)
+
+# from functions import initialize_slab_geometry, evolve_slab_geometry
+# from functions import plot_slab_shape, solve_slab_velocities
+
+
+def initialize_slab_geometry(slab_thickness,init_rad_curvature,init_slab_depth,init_slab_dip):
+	slab_coords=1. # temp
+	return(slab_coords)
+
+
+def evolve_slab_geometry(slab_coords, slab_vels, dt_myrs):
+	slab_coords=1. # temp
+	return(slab_coords)
+
+
+def plot_slab_shape(slab_coords, plot_filename):
+	pass
+
+# def solve_slab_velocities(...):
+# 	...
+# 	return(???)
+
+
