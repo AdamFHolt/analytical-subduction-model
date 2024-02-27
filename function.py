5caadcfab76f4c9ac7015cf3c01fbb9c207ce1e2
AdamFHolt <aholt@rsmas.miami.edu>
2024-02-26 21:37:42 -0500

    added ###

--- function.py
+++ function.py
@@ -2,6 +2,7 @@
 
 # from functions import initialize_slab_geometry, evolve_slab_geometry
 # from functions import plot_slab_shape, solve_slab_velocities
+###
 
 def initialize_slab_geometry(slab_thickness,init_rad_curvature,init_slab_depth,init_slab_dip):
 	slab_coords=1. # temp
